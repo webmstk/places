845ffadb98562a122395e745e3ad3fdbfb433586
Pavel Bakun <webmstk@mail.ru>
2022-12-29 09:20:29 +0300

    added sight_list_screen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(const App());
@@ -9,66 +10,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'App',
-      // home: MyFirstWidget(title: 'MyFirstWidget'),
-      home: MySecondWidget(),
+      home: SightListScreen(),
     );
-  }
-}
-
-class MyFirstWidget extends StatelessWidget {
-  String title;
-  int _counter = 0;
-
-  MyFirstWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    _counter++;
-    print(_counter);
-    print(runtime(context));
-
-    return Container(
-      child: Center(
-        child: Text('Hello!!'),
-      ),
-    );
-  }
-
-  Type runtime(BuildContext context) {
-    return context.runtimeType;
-  }
-}
-
-class MySecondWidget extends StatefulWidget {
-  @override
-  State<MySecondWidget> createState() => _MySecondWidgetState();
-}
-
-class _MySecondWidgetState extends State<MySecondWidget> {
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    _incrementCounter();
-    print(_counter);
-    print(runtime(context));
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  Type runtime(BuildContext context) {
-    return context.runtimeType;
   }
 }
