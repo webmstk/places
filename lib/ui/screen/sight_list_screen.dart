ff30bcf263a2e216834f6bacfb81996df8171fde
Pavel Bakun <webmstk@mail.ru>
2023-01-03 11:31:26 +0300

    module 4 task 4

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,8 +10,31 @@ class SightListScreen extends StatefulWidget {
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  final _backgroundColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(120),
+        child: AppBar(
+          flexibleSpace: const Padding(
+            padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+            child: Text(
+              'Список интересных мест',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          backgroundColor: _backgroundColor,
+          elevation: 0,
+        ),
+      ),
+      backgroundColor: _backgroundColor,
+    );
   }
 }
