36dd7e833957fd1d680c4e4f50bee8c4d78a9731
Pavel Bakun <webmstk@mail.ru>
2023-01-03 11:31:26 +0300

    module 4 task 4

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'App',
+      title: 'Places',
       home: SightListScreen(),
     );
   }
